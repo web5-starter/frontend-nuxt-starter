a781d8e6ea2ded14835aab086cca75918ddd34a1
imba97 <mail@imba97.cn>
2025-01-24 09:33:18 +0800

    chore: dark mode unocss colors

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,6 @@
 import type { PresetOrFactoryAwaitable } from 'unocss'
 
+import { unoColors } from 'uno-colors'
 import {
   defineConfig,
   presetAttributify,
@@ -10,24 +11,32 @@ import {
   transformerVariantGroup
 } from 'unocss'
 
+import presetTheme from 'unocss-preset-theme'
 import { presetUseful } from 'unocss-preset-useful'
+
+const themeColors = {
+  light: {
+    primary: '#4764cf'
+  },
+  dark: {
+    primary: '#4764cf'
+  }
+}
+
+const breakpoints = {
+  'xs': '320px', // Extra small devices (e.g. phones)
+  'sm': '480px', // Small devices (e.g. phones in landscape)
+  'md': '768px', // Medium devices (e.g. tablets)
+  'lg': '1024px', // Large devices (e.g. desktops)
+  'xl': '1280px', // Extra large devices (e.g. large desktops)
+  '2xl': '1536px', // 2x large devices (e.g. large monitors)
+  '3xl': '1920px' // 3x large devices (e.g. ultra-wide monitors)
+}
 
 export default defineConfig({
   theme: {
-    colors: {
-      primary: {
-        DEFAULT: '#64cc96'
-      }
-    },
-    breakpoints: {
-      'xs': '320px', // Extra small devices (e.g. phones)
-      'sm': '480px', // Small devices (e.g. phones in landscape)
-      'md': '768px', // Medium devices (e.g. tablets)
-      'lg': '1024px', // Large devices (e.g. desktops)
-      'xl': '1280px', // Extra large devices (e.g. large desktops)
-      '2xl': '1536px', // 2x large devices (e.g. large monitors)
-      '3xl': '1920px' // 3x large devices (e.g. ultra-wide monitors)
-    }
+    colors: unoColors(themeColors.light),
+    breakpoints
   },
   shortcuts: [
     [/^clickable(-.*)?$/, ([, scale]) => `cursor-pointer transition active:scale${scale || '-95'}`]
@@ -39,7 +48,14 @@ export default defineConfig({
     presetIcons({
       scale: 1.2
     }),
-    presetTypography()
+    presetTypography(),
+    presetTheme({
+      theme: {
+        dark: {
+          colors: unoColors(themeColors.dark)
+        }
+      }
+    }) as PresetOrFactoryAwaitable<object>
   ],
   transformers: [
     transformerDirectives(),
